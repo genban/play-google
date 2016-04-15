6edd5143f1e0614567d7723930d264d1b818d686
joymufeng <joymufeng@163.com>
2016-04-15 17:49:23 +0800

    clean

--- app/Filters.scala
+++ app/Filters.scala
@@ -2,17 +2,17 @@ import javax.inject._
 
 import akka.stream.Materializer
 import play.api.http.HttpFilters
+import play.filters.gzip.GzipFilter
 
 @Singleton
 class Filters @Inject() (implicit val mat: Materializer) extends HttpFilters {
-  override val filters = Nil
-/*  override val filters =
+  override val filters =
     Seq(
       new GzipFilter(shouldGzip = (request, response) => {
           val contentType = response.header.headers.find(t => t._1.trim.toLowerCase == "content-type").map(_._2).getOrElse("").toLowerCase
           contentType.contains("text/html") || contentType.contains("application/json") || contentType.contains("text/javascript")
         }
       )
-    )*/
+    )
 
 }
