57bfc2ae95a382d108352fc318121600473173d0
joymufeng <joymufeng@163.com>
2016-04-28 11:10:53 +0800

    feat: add intercept to filter invalid request

--- app/Filters.scala
+++ app/Filters.scala
@@ -10,7 +10,7 @@ class Filters @Inject() (implicit val mat: Materializer) extends HttpFilters {
     Seq(
       new GzipFilter(shouldGzip = (request, response) => {
           val contentType = response.header.headers.find(t => t._1.trim.toLowerCase == "content-type").map(_._2).getOrElse("").toLowerCase
-          contentType.contains("text/html") || contentType.contains("application/json") || contentType.contains("text/javascript")
+          contentType.contains("text") || contentType.contains("json") || contentType.contains("javascript")
         }
       )
     )
